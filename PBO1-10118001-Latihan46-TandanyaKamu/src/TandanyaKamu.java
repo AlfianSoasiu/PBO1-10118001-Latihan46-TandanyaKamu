13db20dc27fa2be78ecde8b17c39150ecb17b30c
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-11-07 23:56:06 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan46-TandanyaKamu/src/TandanyaKamu.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menampilkan output hitung umur
+ */
+public class TandanyaKamu {
+    
+    public static void main(String[] args) {
+        
+        Umur age = new Umur(2019);
+
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.print("Masukkan Tahun Lahir Anda : ");
+        age.setYearBirth(scn.nextInt());
+        
+        System.out.println("________Hasil Perhitungan Umur________");
+        System.out.println("Tahun Lahir Anda : " + age.getYearBirth(0));
+        System.out.println("Hari ini Tahun : " + age.getYearNow());
+        System.out.println("Umur kamu sampai hari ini adalah "
+                + age.hitungUmur()+ " tahun");
+        System.out.println("Tandanya Kamu : "+ age.tandanyaKamu(age.hitungUmur()));
+    }
+}
